ef3d029970d97aa14d4803a177314ece14cbf4a3
bassarou yacoubou <yacouboubassarou@gmail.com>
2023-07-02 13:15:29 +0100

    doc api

--- config/scribe.php
+++ config/scribe.php
@@ -255,7 +255,7 @@ return [
      * Text to place in the "Introduction" section, right after the `description`. Markdown and HTML are supported.
      */
     'intro_text' => <<<INTRO
-This documentation aims to provide all the information you need to work with our API.
+Cette documentation vise à fournir toutes les informations dont vous avez besoin pour travailler avec notre API.
 
 <aside>Au fur et à mesure que vous faites défiler, vous verrez des exemples de code pour travailler avec l'API dans différents langages de programmation dans la zone sombre à droite (ou dans le cadre du contenu sur mobile).
 Vous pouvez changer la langue utilisée avec les onglets en haut à droite (ou depuis le menu de navigation en haut à gauche sur mobile).</aside>
